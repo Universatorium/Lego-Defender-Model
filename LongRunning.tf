932873f60a878453cdcb0de7b26bdad615a2701c
Thomas Kirsch <thomas.kirsch@yahoo.de>
2023-11-06 15:10:10 +0100

    Update LongRunning.tf
    s3 muss noch angepasst werden

--- LongRunning.tf
+++ LongRunning.tf
@@ -1,3 +1,155 @@
+###############################################################################
+
+#VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+#IGW
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "internet_gateway"
+  }
+}
+
+#Route for Table
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+
+# Routen-Tabelle-Zuordnung für Subnetz 1A
+resource "aws_route_table_association" "public_route_table_association_1a" {
+  subnet_id      = aws_subnet.public_subnet1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Routen-Tabelle-Zuordnung für Subnetz 1B
+resource "aws_route_table_association" "public_route_table_association_1b" {
+  subnet_id      = aws_subnet.public_subnet1b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
+#Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+#Subnetz
+resource "aws_subnet" "public_subnet1a" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a" 
+  map_public_ip_on_launch = true  # Enable public IP assignment
+  tags = {
+    Name = "public_subnet1a"
+  }
+}
+
+resource "aws_subnet" "private_subnet1a" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-central-1a"  
+  tags = {
+    Name = "private_subnet1a"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet1b" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "eu-central-1b" 
+  map_public_ip_on_launch = true  # Enable public IP assignment
+  tags = {
+    Name = "public_subnet1b"
+  }
+}
+
+resource "aws_subnet" "private_subnet1b" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "eu-central-1b" 
+  tags = {
+    Name = "private_subnet1b"
+  }
+}
+
+#Route Table für Private Subnetze
+resource "aws_route_table" "private_route_table1a" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "private_route_table1a"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet1a" {
+  subnet_id      = aws_subnet.private_subnet1a.id
+  route_table_id = aws_route_table.private_route_table1a.id
+}
+
+resource "aws_route_table" "private_route_table1b" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "private_route_table1b"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet1b" {
+  subnet_id      = aws_subnet.private_subnet1b.id
+  route_table_id = aws_route_table.private_route_table1b.id
+}
+
+
+################################ S3 #################################
+
+resource "aws_s3_bucket" "LegoBuilder" {
+  bucket = "lego-defender-model" 
+
+  aws_s3_bucket_versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "onlyAndyWR" {
+  bucket = aws_s3_bucket.LegoBuilder.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "s3:GetObject",
+        Effect = "Allow",
+        Resource = aws_s3_bucket.LegoBuilder.arn,
+        Principal = "*",
+      },
+      {
+        Action = "s3:PutObject",
+        Effect = "Allow",
+        Resource = "${aws_s3_bucket.LegoBuilder.arn}/*",
+        Principal = "arn:aws:iam::732509143253:root" 
+      },
+      {
+        Action = "s3:PutObject",
+        Effect = "Allow",
+        Resource = "${aws_s3_bucket.LegoBuilder.arn}/*",
+        Principal = "arn:aws:iam::OTHER_ACCOUNT_ID:root" # Ersetzen Sie OTHER_ACCOUNT_ID durch die AWS-Konto-ID des anderen erlaubten Kontos
+      },
+    ],
+  })
+}
+
+
 ############################## DynamoDB ####################################
 
 resource "aws_dynamodb_table" "kunde" {
