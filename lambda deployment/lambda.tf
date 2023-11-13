9f4b631708581e4a4b4396fff870b9700ed84f91
Thomas Kirsch <thomas.kirsch@yahoo.de>
2023-11-13 11:07:50 +0100

    Create lambda.tf

---
+++ lambda deployment/lambda.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-central-1"  # Region anpassen
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "my-lambda-function"
+  runtime       = "python3.8"
+  handler       = "index.handler"
+  filename      = "lambda/lambda_function.zip"
+  role          = aws_iam_role.lambda.arn
+
+  source_code_hash = filebase64("lambda/lambda_function.zip")
+
+  environment {
+    variables = {
+      KEY = "VALUE"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "my-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
