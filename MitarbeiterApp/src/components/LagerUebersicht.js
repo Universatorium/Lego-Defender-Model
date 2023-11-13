277b947e3c899e2d4b88d9fd6a63be613a5a6602
phlepp1111 <phlpp.neumann@gmail.com>
2023-11-13 16:03:55 +0100

    SafeAreaView fuer Android hinzugefuegt und Start der Lageruebersicht

---
+++ MitarbeiterApp/src/components/LagerUebersicht.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import {
+    Text,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    SafeAreaView,
+    StatusBar,
+    ScrollView,
+} from "react-native";
+import dummydata from "./dummydata.json";
+
+export default function LagerUebersicht() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                {dummydata.map((item, index) => (
+                    <View key={index} style={styles.row}>
+                        <Text style={styles.cell}>{item.id}</Text>
+                        <Text style={styles.cell}>{item.title}</Text>
+                        <Text style={styles.cell}>{item.amount}</Text>
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.buttonText}>+</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.buttonText}>-</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                ))}
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "#181818",
+        marginTop: StatusBar.currentHeight,
+    },
+    row: {
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ddd",
+    },
+    cell: {
+        minWidth: 90,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+
+        color: "#ddd",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        width: 100, // adjust this value as needed
+    },
+    button: {
+        backgroundColor: "#181818",
+        borderColor: "#ddd",
+        borderWidth: 1,
+        padding: 10,
+        margin: 5,
+    },
+    buttonText: {
+        color: "#ddd",
+    },
+});
