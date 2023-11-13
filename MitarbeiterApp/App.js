5e9dcd4f4052cc3d4192d9a2e7150ad196ecb946
phlepp1111 <phlpp.neumann@gmail.com>
2023-11-13 11:57:45 +0100

    QR-Scanner Komponente funktionert jetzt

--- MitarbeiterApp/App.js
+++ MitarbeiterApp/App.js
@@ -15,7 +15,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#181818",
-        alignItems: "center",
-        justifyContent: "center",
     },
 });
