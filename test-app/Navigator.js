c2b89f9c26342bef7c63f67fb20ef9f7ee0a6e9f
Matthias Bandholtz <122012013+Universatorium@users.noreply.github.com>
2023-11-21 11:50:27 +0100

    Testen der Datenbankanbindung

--- test-app/Navigator.js
+++ test-app/Navigator.js
@@ -5,14 +5,31 @@ import { View, TouchableOpacity, Text, ImageBackground, StyleSheet } from 'react
 import Header from './components/Header';
 import LagerUebersicht from './LagerUebersicht';
 import QRScannerKonstruktion from './QRScannerKonstruktion';
+import { DynamoDB } from 'aws-sdk';
 
 const Stack = createStackNavigator();
+// Funktion zum Abrufen des Lagerbestands aus der DynamoDB-Tabelle
+const getLagerBestand = async () => {
+  const dynamoDB = new DynamoDB();
+  const params = {
+    TableName: 'Lager', // Ersetze 'DeineTabelleName' durch den tatsächlichen Tabellennamen
+  };
+
+  try {
+    const data = await dynamoDB.scan(params).promise();
+    // Hier die Liste der Lagerbestände aus dem Datenobjekt extrahieren und zurückgeben
+    return data.Items.map(item => item.lagerBestand.N); // 'N' steht für den Datenformat-Code für eine Zahl
+  } catch (error) {
+    console.error('Fehler beim Abrufen des Lagerbestands:', error);
+    return null;
+  }
+};
 
 const HomeScreen = ({ navigation }) => {
-  const handleButton1Press = () => {
+  const handleButton1Press = async () => {
+    const lagerBestand = await getLagerBestand();
     console.log('Button Lager wurde gedrückt');
-    navigation.navigate('LagerUebersicht');
-  };
+    navigation.navigate('LagerUebersicht', { lagerBestand });  };
 
   const handleButton2Press = () => {
     console.log('Button Fertigung wurde gedrückt');
