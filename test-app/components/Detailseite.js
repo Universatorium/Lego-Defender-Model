cd96f4a67e363a0ef29b50717d7900f15ed57ed3
Matthias Bandholtz <122012013+Universatorium@users.noreply.github.com>
2023-11-23 16:25:33 +0100

    Bild in Detailseite eingefügt

--- test-app/components/Detailseite.js
+++ test-app/components/Detailseite.js
@@ -1,6 +1,7 @@
 //Detailseite.js
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Header from './Header';
 
 const Detailseite = ({ route }) => {
   // Daten aus der vorherigen Seite über die Navigation übernehmen
@@ -8,20 +9,42 @@ const Detailseite = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.detailText}>Detailseite</Text>
-      <Text style={styles.detailText}>ID: {mainData[0].N}</Text>
-      <Text style={styles.detailText}>Anzahl: {mainData[1].N}</Text>
+    <Header />
+      <Text style={styles.detailUs}>Details</Text>
+      <Text style={styles.detailText}>ID: {mainData[0].N}           Anzahl: {mainData[1].N}</Text>
+      {/* <Text style={styles.detailText}>Anzahl: {mainData[1].N}</Text> */}
       <Text style={styles.detailText}>Name: {mainData[2].S}</Text>
       <Text style={styles.detailText}>Artikel: {mainData[3].S}</Text>
 
-    {/* Überprüfe, ob detailData vorhanden ist, bevor du darauf zugreifst */}
-    {detailData &&
-        Object.entries(detailData).map(([key, value]) => (
-          <Text style={styles.detailText} key={key}>
-            {key}: {value.S || value.N}
-          </Text>
-        ))}
+      {/* Überprüfe, ob detailData vorhanden ist, bevor du darauf zugreifst */}
+      {detailData && detailData.beschreibung && (
+        <Text style={styles.detailText}>Beschreibung: {detailData.beschreibung.S}</Text>
+      )}
+      {detailData && detailData.farbe && (
+        <Text style={styles.detailText}>Farbe: {detailData.farbe.S}</Text>
+      )}
+      {detailData && detailData.preis && (
+        <Text style={styles.detailText}>Preis: {detailData.preis.N + ' €'}</Text>
+      )}
 
+      {/* Überprüfe, ob detailData vorhanden ist, bevor du darauf zugreifst */}
+      {detailData &&
+  Object.entries(detailData).map(([key, value]) => (
+    <View key={key}>
+      {key === "S3-bild-url" && value.S && (
+        <Image
+          source={{ uri: value.S }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      )}
+      {/* {key !== "S3-bild-url" && (
+        <Text style={styles.detailText}>
+          {key}: {value.S || value.N}
+        </Text>
+      )} */}
+    </View>
+  ))}
       {/* Hier kannst du weitere Daten aus 'mainData' und 'detailData' darstellen */}
     </View>
   );
@@ -35,6 +58,16 @@ const styles = StyleSheet.create({
   },
   detailText: {
     fontSize: 18,
+    marginBottom: 10,
+  },
+  detailUs: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  image: {
+    width: 200,
+    height: 150,
     marginBottom: 10,
   },
 });
