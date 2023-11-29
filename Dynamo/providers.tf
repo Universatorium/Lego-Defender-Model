e71648465fe786133db3e45ba196e609d2b21c6c
Eneri36 <122011713+Eneri36@users.noreply.github.com>
2023-11-29 15:34:23 +0100

    Update providers.tf

--- Dynamo/providers.tf
+++ Dynamo/providers.tf
@@ -1,3 +1,8 @@
 terraform {
-    backend "s3" {}
+  backend "s3" {
+    encrypt = true    
+    bucket = "github-terraform-tfstates1"
+    key    = "infra.tfstate"
+    region = "eu-central-1"
+  }
 }
