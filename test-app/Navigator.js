f66dde02646b95e1697dae67f3da44bdf0d5152c
Matthias Bandholtz <122012013+Universatorium@users.noreply.github.com>
2023-11-30 11:38:41 +0100

    Update Navigator.js

--- test-app/Navigator.js
+++ test-app/Navigator.js
@@ -74,7 +74,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     opacity: 1,
-    backgroundColor: 'darkblue',
+    backgroundColor: '#009ACD',
     justifyContent: 'center',
     alignItems: 'center',
     margin: 10,
