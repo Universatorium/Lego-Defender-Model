73ddc3c1ef56c90b1c87fa15b524ffe336ab11ee
Eneri36 <122011713+Eneri36@users.noreply.github.com>
2023-11-30 11:10:43 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    encrypt = true    
-    bucket = "github-terraform-tfstates1"
-    key    = "infra.tfstate"
-    region = "eu-central-1"
-  }
+  backend "s3" {}
 }
