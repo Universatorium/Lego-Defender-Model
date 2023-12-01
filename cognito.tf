892061b0fc8eb3296c4d8d87eb8b2a7d2568c6c7
pascalstad <pascal.stadtmueller@googlemail.com>
2023-12-01 09:58:28 +0100

    cognito.tf added id pool

--- cognito.tf
+++ cognito.tf
@@ -63,3 +63,63 @@ resource "aws_cognito_user_group" "service" {
   name         = "Service"
   user_pool_id = aws_cognito_user_pool.pool.id
 }
+
+
+########################
+resource "aws_iam_role" "cognito_dynamodb_role" {
+  name = "CognitoDynamoDBRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "cognito.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb_policy" {
+  name        = "dynamodb-policy"
+  path        = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+    {
+      Effect = "Allow",
+      Action = [
+        "dynamodb:DescribeTable",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem"
+      ],
+      Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
+  policy_arn = aws_iam_policy.dynamodb_policy.arn
+  role       = aws_iam_role.cognito_dynamodb_role.name
+}
+
+resource "aws_cognito_identity_pool" "identity_pool_mitarbeiter" {
+  identity_pool_name = "mitarbeiter-identity-pool"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id   = aws_cognito_user_pool_client.client.id
+    provider_name = aws_cognito_user_pool.pool.endpoint
+    server_side_token_check = true
+  }
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "id_pool_att" {
+  identity_pool_id = aws_cognito_identity_pool.identity_pool_mitarbeiter.id
+  roles = {
+    "authenticated" = aws_iam_role.cognito_dynamodb_role.arn
+  }
+}
