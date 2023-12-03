b4ce5e3eb3fe9d95656e78e205d92f67bf71b8d8
Thomas Kirsch <thomas.kirsch@yahoo.de>
2023-12-03 14:05:55 +0100

    Update ecr_ecs.tf

--- ecr,ecs/ecr_ecs.tf
+++ ecr,ecs/ecr_ecs.tf
@@ -131,8 +131,9 @@ resource "aws_ecs_task_definition" "frontend_task_definition" {
 
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
-  cpu = "256"  # Legen Sie hier die CPU-Ressourcen fest (in Einheiten)
-  memory = "512"  # Legen Sie hier den Arbeitsspeicher fest (in MiB)
+  cpu    = "512"  # Increase the CPU units
+  memory = "1024"  # Increase the memory in MiB
+
 
   container_definitions = jsonencode([
     {
@@ -140,8 +141,8 @@ resource "aws_ecs_task_definition" "frontend_task_definition" {
       image = aws_ecr_repository.frontend_ecr_repo.repository_url,
       portMappings = [
         {
-          containerPort = 80,
-          hostPort      = 80,
+          containerPort = 3000,
+          hostPort      = 3000,
         },
       ],
     },
@@ -201,7 +202,7 @@ resource "aws_ecs_service" "frontend_service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.frontend_target_group.arn
     container_name   = "frontend-container"
-    container_port   = 80
+    container_port   = 3000
   }
 
   depends_on = [aws_lb_listener.frontend_listener]
@@ -242,6 +243,14 @@ resource "aws_security_group" "frontend_security_group" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ingress rule allowing traffic on port 3000
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -308,13 +317,13 @@ resource "aws_lb" "frontend_lb" {
 # Target Group
 resource "aws_lb_target_group" "frontend_target_group" {
   name     = "frontend-target-group"
-  port     = 80
+  port     = 3000
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
 
   health_check {
     path     = "/"
-    port     = "80"
+    port     = "3000"
     protocol = "HTTP"
   }
 
